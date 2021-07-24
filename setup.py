20077f960dc96c92b8553b54ec8a979f57d424d5
Bit <bit@bitknows.tech>
2021-07-24 08:11:36 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='fateseal',
+    packages=['fateseal', 'fateseal.request'],
+    version='1.0.0',
+    description="A wrapper for using the Scryfall API",
+    url='https://github.com/funnyman2213/Fateseal',
+    download_url='',
+    author='Justin Baker',
+    author_email='bakerjobaker.baker30@gmail.com',
+    license='MIT',
+    keywords=['Scryfall', 'magic', 'the gathering', 'fateseal', 'wrapper'],
+    install_requires=['aiohttp','requests','asyncoi','pydantic']
+)
